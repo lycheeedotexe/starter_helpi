8320715d8d90930844e6738832f5804bc31e32ba
Diana <ddiaz@udel.edu>
2024-04-09 11:19:48 -0400

    progress bar outline added

--- components/progress_bar.tsx
+++ components/progress_bar.tsx
@@ -1,1 +1,25 @@
-//this will be the progress bar component
+import React from "react";
+
+//this will be the progress bar 
+export type ProgressBarProps = {
+    progress: number;
+    progressText?: string;
+  };
+  
+  const ProgressBar = ({ progress, progressText = "" }: ProgressBarProps) => {
+    // Make sure our value stays between 0 and 100.
+    const _progress = Math.min(Math.max(0, progress), 100);
+    return (
+      <div className="flex flex-col items-end justify-start">
+        <div className="w-full border-2 border-indigo-700 h-6 rounded-md">
+          <div
+            className="bg-indigo-500 h-full transition-all duration-250"
+            style={{ width: `${_progress}%` }}
+          ></div>
+        </div>
+        <span>{progressText}</span>
+      </div>
+    );
+  };
+  
+  export default ProgressBar;
