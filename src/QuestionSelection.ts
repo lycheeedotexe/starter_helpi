e625261415a691ba15c14381c46d4103503f3531
Diana <ddiaz@udel.edu>
2024-04-24 21:41:11 -0400

    progressbar

---
+++ src/QuestionSelection.ts
@@ -0,0 +1,163 @@
+//where question selection code will go
+export interface Job {
+        id: number,
+        name: string,
+        relatedDetailedQuestions: number[], //the id of the related questions goes here
+        partitionVector: number[]
+    }
+    
+    export interface BasicQuestion {
+        id: number,
+        name: string,
+        body: string,
+        published: boolean,
+    }
+    
+    export interface DetailedQuestion{
+        id: number,
+        //name: string,
+        body: string,
+        relatedJobs: number[],      //the id of the related jobs goes here
+        published: boolean
+    }
+    
+    interface DataStorage {
+        BASIC_QUESTIONS: BasicQuestion[];
+        DETAILED_QUESTIONS: DetailedQuestion[];
+        JOBS: Job[];
+    }
+    
+    function A_without_B(A: number[], B: number[]){
+        /*
+        returns an array of numbers containing only elements of A that are not in B
+        */
+        const newA = A.filter(element => !B.includes(element))
+        return [...newA];
+    }
+    
+    function jobsInAgreement(data: DataStorage, basicQuestionId: number, userResponse: number): Job[]{ //call the output jobSetA
+        /*
+        data: the JSON file containing all possible questions and jobs
+        basicQuestionId: the ID of the basic question we want to use to partition the job set
+        userResponse: a number between -1 and 1. 
+    
+        If the userResponse is negative, then the jobs in agreement with the user are the ones which are not in agreement with the question. 
+        By definition, these are the Jobs whose ith component of the partition vector is a 0
+    
+        If the userResponse is positive, then the jobs in agreement are the ones whose ith component of the partition vector is a 1
+        */
+       //it might be better in terms of accuracy of results if allow the partition vector to take three values -1, 0, 1. Then, we can pretty much 
+       //have any basic questions we want. We still have to consider the question design, but it may be a better approximation of the user interest
+       //if we can have jobs which are neutral with respect to basic questions. It opens up a lot more design space, i.e. more possible basic questions
+       //since the definition allows for a broader classification of questions
+       if(userResponse === 0){
+         return data.JOBS;
+       } 
+       const responseType = userResponse > 0 ? 1 : -1;
+        return data.JOBS.filter(job => job.partitionVector[basicQuestionId] === responseType);
+    }
+    
+    function jobsNotInAgreement(data: DataStorage, basicQuestionId: number, userResponse:number):Job[]{ //call the output jobSetB
+        /*
+        Since the basic questions split the jobs into two disjoint sets we can just reverse the sign of the user response to get 
+        the jobs which are not in agreement with the user
+        */
+        return jobsInAgreement(data, basicQuestionId, -1*userResponse);
+    }
+    
+    export function DetailedQuestionsInAgreement(jobSet:Job[]): number[]{
+        var questionSet = new Set<number>();
+        jobSet.forEach(job => {
+                job.relatedDetailedQuestions.filter((questionId: number) => questionId > 50).forEach(questionId => questionSet.add(questionId))
+        })
+        return Array.from(questionSet); 
+    }
+    
+    function makeUniformMeasure(numDetailedQuestions: number): number[]{
+        /*
+        returns a dictionary such that every key corresponds to 1/numDetailedQuestions
+        */
+        const entryVal = 1/numDetailedQuestions;
+        return new Array(numDetailedQuestions).fill(entryVal);
+    }
+    
+    function updateMeasure(prevMeasure: number[], userResponse: number, jobSetA: Job[], jobSetB: Job[]): number[] {
+        if (userResponse === 0) {
+            return [...prevMeasure];
+        }
+        if(userResponse < 0){
+            return updateMeasure(prevMeasure, -1*userResponse, jobSetB, jobSetA);
+        }
+    
+        const tempA = DetailedQuestionsInAgreement(jobSetA);
+        const tempB = DetailedQuestionsInAgreement(jobSetB);
+    
+        const indexSetA = A_without_B(tempA, tempB);
+        const indexSetB = A_without_B(tempB, tempA);
+    
+        const measureToBeRemoved = prevMeasure.reduce((acc, current, index) => {
+            return indexSetB.includes(index + 1) ? acc + current : acc; 
+        }, 0) * userResponse; 
+    
+        return prevMeasure.map((entry, index) => {
+            if (indexSetA.includes(index + 1) || indexSetB.includes(index + 1)) {
+                if (indexSetA.includes(index + 1)) {
+                    return entry + (measureToBeRemoved / indexSetA.length);
+                } else {
+                    return entry * (1 - userResponse);
+                }
+            }
+            return entry; 
+        });
+    }
+    
+    export function constructFinalMeasure(data: DataStorage, responseVector: number[]){
+        const baseMeasure = makeUniformMeasure(data.DETAILED_QUESTIONS.length);
+        var iterativeMeasure = [...baseMeasure];
+        for(var i = 0; i < responseVector.length; i++){
+            //console.log(`\n measure says P(D) = ${iterativeMeasure.reduce((acc, entry, index) => acc + entry, 0 )}\n`)
+            iterativeMeasure = updateMeasure(iterativeMeasure, responseVector[i], jobsInAgreement(data,i,responseVector[i]), jobsNotInAgreement(data,i,responseVector[i]));
+        };
+        return [...iterativeMeasure];
+    }
+    
+    export function constructDistribution(measure: number[]){
+        /* 
+        measure is an array of numbers. measure[i] corresponds to the probability that detailed question i is chosen. We construct the distribution
+        by mapping the sum of the elements A[0]+...+A[i] to B[i]
+        */
+        return measure.map((element, i) => measure.slice(0,i+1).reduce((sum, current) => sum + current, 0));
+    }
+    
+    function sampleQuestion(data: DataStorage, dist: number[]){
+    
+        const r = Math.random();
+        return dist.findIndex(element => element >= r ) + 1;
+    }
+    
+    export function publishDetailedQuestions(data: DataStorage, responseVector: number[], numQuestions: number): DetailedQuestion[]{
+        /*
+            data: a copy of the JSON file where all of our questions are stored
+            responseVector: a vector of numbers corresponding to the user responses to the detailed questions
+            numQuestions: the number of detailed questions we want to include in our quiz
+            
+            This function uses the constructDistribution and constructFinalMeasure functions to obtain a probability 
+            distribution on the set of detailed questions. It then generates random numbers and uses them to sample question
+            IDs from the probability distribution until we reach the desired number of questions.
+    
+            It sets the published field of all the questions with a sampled ID to true, and then returns a filtered array
+            where all the unpublished detailed questions are removed. This output is then fed to our DetailedQuestionsPage.tsx file.
+        */
+        const copyOfData = JSON.parse(JSON.stringify(data));
+        const dist = constructDistribution(constructFinalMeasure(copyOfData, responseVector));
+        var numSampled = 0;
+        while(numSampled < numQuestions){
+            const currentId = sampleQuestion(copyOfData, dist);
+            const question = copyOfData.DETAILED_QUESTIONS.find((q: DetailedQuestion) => q.id === currentId);
+            if(question && question.published === false){
+                question.published = true;
+                numSampled = numSampled + 1;
+            }
+        }
+        return copyOfData.DETAILED_QUESTIONS.filter((q:DetailedQuestion) => q.published === true);
+    }
