931faf082f4e63c33b24393f6faf3cc3b858c95d
Diana <ddiaz@udel.edu>
2024-05-11 17:50:51 -0400

    ignore

--- src/submitButtons/submitDetailed.tsx
+++ src/submitButtons/submitDetailed.tsx
@@ -17,19 +17,6 @@ export function SubmitDetailed(): JSX.Element{
     const recommendations = recommendJobs(dataCopy, responseDict, sampledKeys);
 
     const getResponseFunction = async() => {
-        // const response = await openai.chat.completions.create({
-        //     messages: [{"role": "system", "content": `You are a robot career counselor named Perceptron, with the ability to peer into college student's souls and give the best career advice. Provide output in valid JSON. The data schema should be like this: ${resultsDetailed}`},
-        //         {"role": "user", "content": `Generate JSON file for "${recommendations.map((j:Job) => (j.name + ", "))}" that includes the job title with the key "title", a short 1-3 sentence description of each job with the key "description", an entry salary with the key "entrySalary", a median salary with the key "medianSalary", and as an array a career path that tells the steps to achieving each job with the key "careerPath".`}],
-        //     model: "gpt-4-turbo",
-        //     response_format:{"type": "json_object"}
-        // })
-        // console.log(response);
-        // const results = response.choices[0].message.content
-        // if (results !== null) {
-        // console.log(results[0]);
-        // } else {
-        //     console.log(resultsDetailed.CAREER_RESULTS);
-        // }
         for(var i = 0; i < 3; i++) {
             resultsDetailed.CAREER_RESULTS[i].title = recommendations[i].name;
             const question = [`Generate a 1-3 sentence job description for "${recommendations[i].name}".`,
