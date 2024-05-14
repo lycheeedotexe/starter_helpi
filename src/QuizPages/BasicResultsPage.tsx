7f7ff4b348424b24389a74e1e94b7af5a28e8590
Sil0 <silo@udel.edu>
2024-05-13 23:00:26 -0400

    for bar graph testing

--- src/QuizPages/BasicResultsPage.tsx
+++ src/QuizPages/BasicResultsPage.tsx
@@ -4,6 +4,9 @@ import { constructFinalMeasure } from "../QuizFunctions/QuestionSelection";
 import questions from "../data/questions.json"
 import { getResponseVector } from "../QuizFunctions/getResponseVector";
 import { DetailedQuestionsInAgreement, Job } from "../QuizFunctions/QuestionSelection";
+import BarGraph from "../components/BarGraph";
+import {DataItem} from "../components/BarGraph"
+import { FormatBasicResults } from "../components/formatBasicResults";
 
 const jobClusters: Record<number, string> = {
     1:"Technology",
@@ -40,13 +43,32 @@ const BasicResultsPage = () => {
     console.log(`topClusterID = ${topClusterID}`)
     const clusterKeys = [5*topClusterID + 1, 5* topClusterID+2, 5*topClusterID+3,5*topClusterID+4,5*topClusterID+5];
     const relatedJobs = clusterKeys.map((k:number) => dataCopy.JOBS[k]);
+
+   
+    const normalizedClusterProbability = (k: number): number =>{
+        const clusterQuestionProbability = [1,2,3,4,5,6,7,8,9,10].map((n: number) => clusterProbability(n)).reduce((acc, entry)=> acc + entry, 0)
+        return clusterProbability(k) / clusterQuestionProbability;
+    }
     console.log(`clusterKeys = ${clusterKeys}`)
     console.log(`relatedJobs = ${relatedJobs}`)
     
+    const dataforGraph = rankedClusters.map((k:number): DataItem => {
+            const clusterData = {name:jobClusters[k], value:normalizedClusterProbability(k)}
+            return clusterData;
+
+    } )
 
 
     return(
         <div>
+            <div>
+                <h1>Your scores for all clusters: </h1>
+                <BarGraph
+                data={dataforGraph}
+                color={'blue'}
+                />
+
+            </div>
              <h1>Your top cluster is {jobClusters[topClusterID]}. You may be well suited for the following careers</h1>
             {
             
